92a290a68ecf5eef8699fadeffffcfdbacebf4b3
henrymedeiros <henrymedeiros77@gmail.com>
2023-03-10 15:33:09 -0300

    req.txt

--- main.py
+++ main.py
@@ -40,15 +40,15 @@ def send_mail(product, user_email, password):
     s.starttls()
     s.login(user_email, password)
     s.sendmail(user_email, user_email, msg.as_string().encode('utf-8'))
-    print('email enviado')
 
-   
+
 for link in links:
     r = session.get(link)
     page_html = r.html.render(sleep=2)
     product = get_product_data(link,page_html)
     is_available = check_availability(link,page_html)
+    print(product, '\n', is_available)
     if(is_available):
         send_mail(product, os.getenv('USER_EMAIL'), os.getenv('APP_PASSWORD'))
-    print(product)
-    print(is_available)
+    
+
