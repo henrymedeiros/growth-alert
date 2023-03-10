3f40c7c6de421cd3835c8b8c55fd3edb7e02c4f3
henrymedeiros <henrymedeiros77@gmail.com>
2023-03-10 15:25:29 -0300

    first version

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-from requests_html import HTMLSession
+import os
 import smtplib
-
 import email.message
+from dotenv import load_dotenv
+from requests_html import HTMLSession
 
+load_dotenv()
 session = HTMLSession()
 
-links = [
-    'https://www.gsuplementos.com.br/creatina-monohidratada-250gr-growth-supplements-p985931',
-    'https://www.gsuplementos.com.br/pasta-de-amendoim-integral-torrado-1kg-growth-supplements-p988045'
-]
+def read_links_from_txt():
+    with open('links.txt', 'r') as f:
+        links = f.read().splitlines()
+        return links
 
-
+links = read_links_from_txt()
 
 def get_product_data(link, page_html):
+    name = r.html.find('.topoDetalhe-boxRight-nome', first=True)
+    price = r.html.find('[data-desconto-boleto-valor]', first=True)
+    if name is None or price is None:
+        print('erro ao pegar infos do produto')
     product = {
-        'name': r.html.find('.topoDetalhe-boxRight-nome', first=True).text,
-        'price': r.html.find('[data-desconto-boleto-valor]', first=True).text
+        'name': name.text,
+        'price': price.text
     }
     return product
 
@@ -24,11 +30,25 @@ def check_availability(link, page_html):
     if not_available_buy_button:
         return False
     return True
+
+def send_mail(product, user_email, password):
+    msg = email.message.Message()
+    msg['Subject'] = f"[ALERTA GROWTH] - {product['name']}"
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(f"O item {product['name']} está disponível por {product['price']}! <br> Acesse o link: " + link)
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+    s.login(user_email, password)
+    s.sendmail(user_email, user_email, msg.as_string().encode('utf-8'))
+    print('email enviado')
+
    
 for link in links:
     r = session.get(link)
-    page_html = r.html.render(sleep=1)
+    page_html = r.html.render(sleep=2)
     product = get_product_data(link,page_html)
     is_available = check_availability(link,page_html)
+    if(is_available):
+        send_mail(product, os.getenv('USER_EMAIL'), os.getenv('APP_PASSWORD'))
     print(product)
     print(is_available)
